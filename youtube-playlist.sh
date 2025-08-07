ca8cc501f070f869218376bb78642484aa5ac1cf
jadissa <5577795+jadissa@users.noreply.github.com>
2025-08-07 14:26:09 -0600

    variable playlist

--- youtube-playlist.sh
+++ youtube-playlist.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ -z "$1" ]]; then
+	echo 'Supply a playlist...exiting'
+	exit
+fi
+
 # Install
 curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o ~/.local/bin/yt-dlp
 chmod a+rx ~/.local/bin/yt-dlp  # Make executable
@@ -8,7 +13,7 @@ chmod a+rx ~/.local/bin/yt-dlp  # Make executable
 ./yt-dlp -U
 
 # Download playlist
-./yt-dlp https://music.youtube.com/playlist?list=PLOfTY28w1MFkfSrWF3WjdIe5Igk4wMEqm
+./yt-dlp $1
 
 # Move playlist
 mv *.mp4 /Volumes/LaCie/mus
