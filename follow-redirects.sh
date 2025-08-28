6037b824367168bf8405880225b1bfbc394643cc
jadissa <5577795+jadissa@users.noreply.github.com>
2025-08-27 21:43:47 -0600

    redirects

--- follow-redirects.sh
+++ follow-redirects.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cmd='curl --verbose --connect-timeout 15 --location --head'
+cmd='curl --verbose --connect-timeout 30 --location -s -o /dev/null -w "%{url_effective}\n"'
 echo $cmd $1
 $cmd $1
 echo $cmd $1
