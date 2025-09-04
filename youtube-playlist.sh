86e5d67ea69f8f30600c443f167c2d8a9770fd18
jadissa <5577795+jadissa@users.noreply.github.com>
2025-09-03 18:37:30 -0600

    refactored a bit

--- youtube-playlist.sh
+++ youtube-playlist.sh
@@ -5,6 +5,11 @@ if [[ -z "$1" ]]; then
 	exit
 fi
 
+if [[ -z "$2" ]]; then
+	echo 'Supply an output directory...exiting'
+	exit
+fi
+
 # Install
 curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o ~/.local/bin/yt-dlp
 chmod a+rx ~/.local/bin/yt-dlp  # Make executable
@@ -12,11 +17,14 @@ chmod a+rx ~/.local/bin/yt-dlp  # Make executable
 # Update
 ~/.local/bin/./yt-dlp -U
 
-# Download playlist
-~/.local/bin/./yt-dlp $1
-
-# Move playlist
-mkdir -p /Volumes/LaCie/mus && mv *.mp4 /Volumes/LaCie/mus
+~/.local/bin/./yt-dlp \
+    --ignore-errors \
+    --extract-audio \
+    --audio-format "mp3" \
+    --audio-quality 0 \
+    --yes-playlist \
+    -o "$2/%(playlist_index)s - %(title)s.%(ext)s" \
+    "$1"
 
 # List playlist
-ls -ltrah /Volumes/LaCie/mus
+ls -ltrah $2
